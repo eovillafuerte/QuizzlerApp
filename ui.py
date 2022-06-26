b58e1d8f79c2d00fe5a2c93f9f58624f6724cbf7
eovillafuerte <eovillafuerte@outlook.com>
2022-06-26 06:34:40 -0600

    score label

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import *
 
 THEME_COLOR = "#375362"
 
@@ -7,5 +7,9 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
 
         self.window.mainloop()
