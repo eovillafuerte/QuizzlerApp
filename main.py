97bc79f7317743069454686b0d63cfae80182a13
eovillafuerte <eovillafuerte@outlook.com>
2022-06-26 06:25:33 -0600

    Tk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-
+from ui import QuizInterface
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -11,7 +11,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-
+quiz_ui = QuizInterface()
 while quiz.still_has_questions():
     quiz.next_question()
 
