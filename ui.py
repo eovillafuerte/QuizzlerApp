fe6a595ac786831e038d03fd9ab39da15b7e4af9
eovillafuerte <eovillafuerte@outlook.com>
2022-06-26 16:30:01 -0600

    width fix

--- ui.py
+++ ui.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
+
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self, quiz_brain):
+    def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
@@ -16,6 +18,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Text",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -31,7 +34,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
