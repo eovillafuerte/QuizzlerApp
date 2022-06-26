a3b62c6cabdb2e67dc12d7c3bd80644f6cb24f8b
eovillafuerte <eovillafuerte@outlook.com>
2022-06-26 06:40:10 -0600

    canvas

--- ui.py
+++ ui.py
@@ -11,5 +11,9 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Some Question Text", fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspab=2)
+
 
         self.window.mainloop()
